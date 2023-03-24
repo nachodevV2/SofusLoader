9c8b4856482ff255fb7c75974513432e70adea3a
nachodevV2 <126583853+nachodevV2@users.noreply.github.com>
2023-03-24 18:11:32 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,21 +1,8 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Azura-Technology/Azura-Hub/main/BetaUpdateVapeUiLib"))()
 
-local Window = Library.CreateLib("Sofus Loader", "Synapse")
+local win = lib:Window("Sofus Loader",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl
 
-  local Tab = Window:NewTab("Home")
-  Section:NewLabel("This is the loader for all the Sofus scripts. Look at the tabs on the left to see what you need, then click the button to load it!")
+  lib:Notification("Alert", "Sofus Loader", "Injected!) 
   
-  local Tab = Window:NewTab("Games")
-  Section:NewLabel("Loads Sofus scripts for specific games.")
-  
-    Section:NewButton("BedWars Sofus [PC]", "Bedwars Sofus for PC", function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/nachodevV2/Sofus.GG/main/MainScript.lua", true))()
-    end)
-    
-    Section:NewButton("BedWars Sofus [Mobile]", "In Development.", function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/nachodevV2/Sofus.GG/main/MainScript.lua", true))()
-    end)
-    
-    Section:NewButton("GandmaWare V3", "A Vape Config (PistonWare based)", function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/nachodevV2/Sofus.GG/main/MainScript.lua", true))()
-    end)
+-- tab:Tab(title <string>)
+local tab = win:Tab("Sofus Scripts")
